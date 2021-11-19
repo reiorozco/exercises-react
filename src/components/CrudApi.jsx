7e0c8d4610daffbb78fcc3bce48ef9bf534b9a58
Rei Orozco <rfoc15@gmail.com>
2021-11-19 12:41:00 -0500

    Initialize the state with AJAX

--- src/components/CrudApi.jsx
+++ src/components/CrudApi.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { helpHttp } from "../helpers/helpHttp";
 import CrudForm from "./CrudForm";
 import CrudTable from "./CrudTable";
 
 export default function CrudApi() {
   const [database, setDatabase] = useState([]);
   const [dataToEdit, setDataToEdit] = useState(null);
+
+  let api = helpHttp();
+  let url = "http://localhost:5000/mockData";
+
+  useEffect(() => {
+    api.get(url).then((res) => {
+      // console.log(res);
+      if (!res.err) {
+        setDatabase(res);
+      } else {
+        setDatabase(null);
+      }
+    });
+  }, []);
 
   const createData = (data) => {
     // console.log(data);
