73e81a3e86d7f5056b2369397675dd5960c66fe1
Rei Orozco <rfoc15@gmail.com>
2021-11-18 21:48:22 -0500

    Creating an API with JSON Server

---
+++ src/components/CrudApi.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import CrudForm from "./CrudForm";
+import CrudTable from "./CrudTable";
+
+export default function CrudApi() {
+  const [database, setDatabase] = useState([]);
+  const [dataToEdit, setDataToEdit] = useState(null);
+
+  const createData = (data) => {
+    // console.log(data);
+    // data.id = Date.now();
+    data.id = database.length + 1;
+    setDatabase([...database, data]);
+  };
+
+  const updateData = (data) => {
+    let newData = database.map((el) => (el.id === data.id ? data : el));
+    setDatabase(newData);
+  };
+
+  const deleteData = (id) => {
+    let isDelete = window.confirm(
+      `Estas seguro de eliminar el registro con el id "${id}"`
+    );
+
+    if (isDelete) {
+      let newData = database.filter((el) => el.id !== id);
+      setDatabase(newData);
+    } else {
+      return;
+    }
+  };
+
+  return (
+    <div>
+      <h2>CRUD API</h2>
+      <article className="grid-1-2">
+        <CrudForm
+          createData={createData}
+          updateData={updateData}
+          dataToEdit={dataToEdit}
+          setDataToEdit={setDataToEdit}
+        />
+        <CrudTable
+          data={database}
+          setDataToEdit={setDataToEdit}
+          deleteData={deleteData}
+        />
+      </article>
+    </div>
+  );
+}
